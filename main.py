06703c7e8c1abaa2652b80667aa84af76b9cd4bb
Nikolay <nikswat@mail.ru>
2018-11-21 17:17:14 +0700

    добавил тестовый просчёт ядра фильтра Габора

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def skeletization(img):
     size = np.size(img)
     skel = np.zeros(img.shape,np.uint8)
 
-    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (4, 4))
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
     done = False
 
     while not done:
@@ -25,15 +25,29 @@ def skeletization(img):
         img = eroded.copy()
 
         zeros = size - cv2.countNonZero(img)
-        if zeros==size:
+        if zeros == size:
             done = True
     return skel
 
+
+# оргигинальное изображение
 originalImage = cv2.imread('./fingers/101_2.tif', 0)
+# бинаризируем изображение
 binaryImg = cv2.adaptiveThreshold(originalImage, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+# нормализуем изображение с помощью вычисления гистограмы
+hist = cv2.calcHist([binaryImg], [0], None, [256], [0, 256])
+# поиск главных компонент методом PCA
+mean, eigenVectors = cv2.PCACompute(hist, np.mean(hist, 0))
+print(mean, eigenVectors)
+# улучшение изображения с помощью фильтра Габора
+g_kernel = cv2.getGaborKernel((11, 11), 8.0, np.pi/4, 10.0, 0.5, 0)
+filtered_image = cv2.filter2D(binaryImg, cv2.CV_8UC3, g_kernel)
 
-skelet = skeletization(binaryImg);
+skelet = skeletization(filtered_image)
+cv2.imshow('gabor kernel (resized)', skelet)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-images = [originalImage, binaryImg, skelet]
-titles = ['Оригинал', 'Бинарное', 'skel']
+images = [originalImage, binaryImg, skelet, filtered_image]
+titles = ['Оригинал', 'Бинарное', 'skel', 'gabor']
 viewResults(len(images), images, titles)
